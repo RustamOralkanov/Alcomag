5f36faf437fe99a594b09253012899beb2dda518
Rustam Oralkanov <rustamoralkanov@MacBook-Pro-Rustam.local>
2024-03-05 22:23:25 +0500

    html

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,10 +1,6 @@
 const swiper = new Swiper(".banners-carousel", {
     loop: true,
 
-    pagination: {
-        el: ".swiper-pagination",
-    },
-
     navigation: {
         nextEl: ".banners-button-next",
         prevEl: ".banners-button-prev",
@@ -19,6 +15,27 @@ const cardsSwiper = new Swiper(".cards-carousel", {
         nextEl: ".cards-btn-next",
         prevEl: ".cards-btn-prev",
     },
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+            slidesPerView: 2,
+            spaceBetween: 10,
+        },
+        // when window width is >= 480px
+        767: {
+            slidesPerView: 2,
+            spaceBetween: 10,
+        },
+        // when window width is >= 640px
+        992: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+        },
+        1024: {
+            slidesPerView: 4,
+            spaceBetween: 20,
+        },
+    },
 });
 
 const cardsTwoSwiper = new Swiper(".cards-carousel-second", {
@@ -28,6 +45,27 @@ const cardsTwoSwiper = new Swiper(".cards-carousel-second", {
     navigation: {
         nextEl: ".cards-btn-next-second",
         prevEl: ".cards-btn-prev-second",
+    },
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+            slidesPerView: 2,
+            spaceBetween: 10,
+        },
+        // when window width is >= 480px
+        767: {
+            slidesPerView: 2,
+            spaceBetween: 10,
+        },
+        // when window width is >= 640px
+        992: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+        },
+        1024: {
+            slidesPerView: 4,
+            spaceBetween: 20,
+        },
     },
 });
 
@@ -39,6 +77,27 @@ const cardsThirdSwiper = new Swiper(".cards-carousel-third", {
         nextEl: ".cards-btn-next-third",
         prevEl: ".cards-btn-prev-third",
     },
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+        },
+        // when window width is >= 480px
+        567: {
+            slidesPerView: 2,
+            spaceBetween: 10,
+        },
+        // when window width is >= 640px
+        992: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+        },
+        1024: {
+            slidesPerView: 4,
+            spaceBetween: 20,
+        },
+    },
 });
 
 const news = new Swiper(".news-carousel", {
@@ -48,5 +107,26 @@ const news = new Swiper(".news-carousel", {
     navigation: {
         nextEl: ".news-btn-next",
         prevEl: ".news-btn-prev",
+    },
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+            slidesPerView: 2,
+            spaceBetween: 10,
+        },
+        // when window width is >= 480px
+        767: {
+            slidesPerView: 2,
+            spaceBetween: 10,
+        },
+        // when window width is >= 640px
+        992: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+        },
+        1024: {
+            slidesPerView: 4,
+            spaceBetween: 20,
+        },
     },
 });
